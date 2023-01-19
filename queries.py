1aa29fea6076150e241c69044cf3cd0621d1e869
rababe azil <azilrababe@gmail.com>
2023-01-19 16:09:50 +0100

    ...

--- queries.py
+++ queries.py
@@ -1,27 +1,40 @@
 # pylint: disable=missing-docstring, C0103
 
+
 def directors_count(db):
     # return the number of directors contained in the database
-    pass  # YOUR CODE HERE
-
+    query = "SELECT count(*) FROM directors "
+    db.execute(query)
+    results = db.fetchall()
+    return results[0][0]
 
 def directors_list(db):
     # return the list of all the directors sorted in alphabetical order
-    pass  # YOUR CODE HERE
-
+    query = "select * from directors order by name"
+    db.execute(query)
+    results = db.fetchall()
+    return [x[0] for x in results]
 
 def love_movies(db):
     # return the list of all movies which contain the exact word "love"
     # in their title, sorted in alphabetical order
-    pass  # YOUR CODE HERE
-
+    query = "SELECT * from movies where upper(title) like '% LOVE %' ORDER by title"
+    db.execute(query)
+    results = db.fetchall()
+    return results
 
 def directors_named_like_count(db, name):
     # return the number of directors which contain a given word in their name
-    pass  # YOUR CODE HERE
+    query = f"SELECT  count(*) from directors where upper(name) like '% upper({name}) %'"
+    db.execute(query)
+    results = db.fetchall()
+    return results[0][0]
 
 
 def movies_longer_than(db, min_length):
     # return this list of all movies which are longer than a given duration,
     # sorted in the alphabetical order
-    pass  # YOUR CODE HERE
+    query = f"select * from movies WHERE minutes > {min_length} ORDER by title"
+    db.execute(query)
+    results = db.fetchall()
+    return [x[0] for x in results]
